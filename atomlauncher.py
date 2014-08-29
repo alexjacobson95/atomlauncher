4bcbdc8c2dd5c4f34049a345e4642f843e98a639
Alex Jacobson <alexjacobson95@gmail.com>
2014-08-29 00:40:32 -0700

    added basic window and centered it

--- atomlauncher.py
+++ atomlauncher.py
@@ -1,1 +1,13 @@
-print 'test'
+import wx
+
+class GUI(wx.Frame):
+	def __init__(self, parent, id, title):
+		wx.Frame.__init__(self, parent, id, title,  size=wx.Size(450, 200), style=wx.NO_BORDER)
+		#self.SetBackgroundColour("blue")
+		self.Centre();
+
+
+app = wx.App(False)
+frame = GUI(None, -1, "Atome Launcher")
+frame.Show(True)
+app.MainLoop()
