8b1a76321e35ed7eafc003c16a1581f7c3510056
Alex Jacobson <alexjacobson95@gmail.com>
2014-08-29 22:13:07 -0700

    added icon and basic hotkey is functioning

--- atomlauncher.py
+++ atomlauncher.py
@@ -1,4 +1,7 @@
+#win32con comes from: http://sourceforge.net/projects/pywin32/files/pywin32/
+
 import wx
+import win32con
 
 TRAY_TOOLTIP = 'System Tray Demo'
 TRAY_ICON = 'atom.png'
@@ -9,6 +12,8 @@ def create_menu_item(menu, label, func):
 	menu.AppendItem(item)
 	return item
 
+def toggleWindow():
+		mainWindow.Show(not mainWindow.IsShown())
 
 class Window(wx.Frame):
 	def __init__(self, parent, id, title):
@@ -16,11 +21,27 @@ class Window(wx.Frame):
 		self.SetBackgroundColour("red")
 		self.Centre()
 
+		self.regHotKey()
+		self.Bind(wx.EVT_HOTKEY, self.handleHotKey, id=self.hotKeyId)
+
+	def regHotKey(self):
+		self.hotKeyId = 100
+		self.RegisterHotKey(self.hotKeyId, win32con.MOD_ALT, win32con.VK_RETURN)
+
+	def handleHotKey(self, event):
+		toggleWindow()
+
+		if mainWindow.IsShown():
+			
+
+
 class TaskBarIcon(wx.TaskBarIcon):
 	def __init__(self):
 		super(TaskBarIcon, self).__init__()
 		self.set_icon(TRAY_ICON)
 		self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+		toggleWindow()
 
 	def CreatePopupMenu(self):
 		menu = wx.Menu()
@@ -34,10 +55,10 @@ class TaskBarIcon(wx.TaskBarIcon):
 		self.SetIcon(icon, TRAY_TOOLTIP)
 
 	def on_left_down(self, event):
-		print 'Tray icon was left-clicked'
+		toggleWindow()
 
 	def on_hello(self, event):
-		mainWindow.Show(True)
+		toggleWindow()
 
 	def on_exit(self, event):
 		mainWindow.Destroy()
